b6cbc3741a29b510888f6d1c8d788e5d070f3631
Julien Poyard <jpoy77@yahoo.com>
2021-04-04 22:21:49 +0200

    cv to PDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { LanguageService } from './core/services/language.service';
-import { getMockSkills } from './features/viewer/mock/skill.mock.spec';
 import { SupportedLanguageEnum } from './model/language';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'mcv-root',
@@ -19,5 +19,20 @@ export class AppComponent {
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use(SupportedLanguageEnum.english);
+  }
+
+  public openPDF(data: HTMLElement): void {
+    html2canvas(data).then(canvas => {
+
+      let fileWidth = 208;
+      let fileHeight = canvas.height * fileWidth / canvas.width;
+
+      const FILEURI = canvas.toDataURL('image/png')
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
+
+      PDF.save('angular-demo.pdf');
+    });
   }
 }
