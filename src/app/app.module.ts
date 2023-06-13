f9591c4fd44bf6577b14d640b80ce735557a94aa
Julien Poyard <julien.poyard@evooq.ch>
2023-06-13 16:35:17 +0200

    define deploy config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import localeEn from '@angular/common/locales/en';
 import localeFr from '@angular/common/locales/fr';
 import { SupportedLanguageEnum } from './model/language';
+/*
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+*/
 
 registerLocaleData(localeFr, SupportedLanguageEnum.french);
 registerLocaleData(localeEn, SupportedLanguageEnum.english);
